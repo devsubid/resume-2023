12b1e911393038fec316d87ccbce169717a0f339
SUBID DAS <itsmesubid@gmail.com>
2023-01-11 17:49:59 +0530

    Add `resume`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    --light-color: 255, 255, 255;
+    --dark-color: 49, 49, 49;
+    --primary-color: 251, 195, 27;
+    --secondary-color: 0, 122, 163;
+  }
+  *{
+    font-family: var(--font-poppins);
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+  body {
+    margin-block: 4rem;
+    background-color: rgb(var(--light-color));
+    color: rgb(var(--dark-color));
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+    min-height: 100vh;
+  }
+  .container {
+  margin-inline: auto;
+  width: min(90%, 70rem);
+}
+a{
+  color: inherit;
+  text-decoration: none;
+}
+`;
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={poppins.variable}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </div>
+  );
 }
